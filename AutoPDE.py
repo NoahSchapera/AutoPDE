5c49d9927b876eac5cf7eba6f5f588b05eb250e4
NoahSchapera <43219611+NoahSchapera@users.noreply.github.com>
2023-06-24 20:06:59 -0400

    Added interface testing

--- AutoPDE.py
+++ AutoPDE.py
@@ -29,11 +29,7 @@ def collectData(wavelength_select, wavelength, wavelength_index, voltage_400, vo
             os.system("~/PDEDAQ/PDEDAQ -of " +"../"+ SN+"/"+SIPM+"_"+wl_choice+"nm_"+v+"V_"+date+ " -w " + wl_index_choice + " -inputrange 1")
         print(" ")
 
-
-print('Welcome to the AutoPDE workflow')
-print("V1.0 - 23 / 06 / 2023")
-print("by Eden Schapera")
-
+#initializing variables
 
 testing = True
 
@@ -41,21 +37,39 @@ date = "23062023"
 SIPM = "S14161-6050HS-04"
 SN = "SN14_v2"
 
-date = input("Date DDMMYYYY : ")
-SN = input("Directory Name (SN) : ")
-
-
 wavelength = ["400", "500", "589"]
 wavelength_index = ["1", "3", "4"]
 voltage_400 = ["41.0", "41.5", "42.0", "43.0", "45.0"]
 voltage_500_589 = ["41.0", "41.5", "42.0", "42.5"]
 
+# begin program
+print('Welcome to the AutoPDE workflow')
+print("V1.1 - 23 / 06 / 2023")
+print("by Eden Schapera")
+print("----------------------------")
+test_input = input("Testing? y/n : ")
+
+# testing parser
+if test_input == "y":
+    testing = True
+else: 
+    testing = False
+
+
+# input vals
+print(" ")
+date = input("Date DDMMYYYY : ")
+SN = input("Directory Name (SN) : ")
+
+
 wl_input = int(input("Select wavelength: 400nm (0), 500nm (1), 589nm (2), all (3) : "))
 
+# if all, repeat for all wavelengths
 if wl_input == 3:
     for wl in range(len(wavelength)):
         collectData(wl, wavelength, wavelength_index, voltage_400, voltage_500_589, testing)
-        
+
+#else, only do one wavelength
 else: 
     collectData(wl_input, wavelength, wavelength_index, voltage_400, voltage_500_589, testing)
         
